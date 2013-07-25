dfe7125510f340b3f60e23702598a97a03fcf877
web-studio <vanechka_petrov_1996@list.ru>
2013-07-25 20:32:01 +0400

    + User

---
+++ www/protected/views/user/_view.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $this UserController */
+/* @var $data User */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
+	<?php echo CHtml::encode($data->password); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('register_time')); ?>:</b>
+	<?php echo CHtml::encode($data->register_time); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('update_time')); ?>:</b>
+	<?php echo CHtml::encode($data->update_time); ?>
+	<br />
+
+
+</div>
