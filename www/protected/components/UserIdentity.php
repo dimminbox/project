c8c85922ca94350754939f062378218db67b9673
web-studio <vanechka_petrov_1996@list.ru>
2013-07-25 21:03:23 +0400

    + Добавил авторизацию по email

--- www/protected/components/UserIdentity.php
+++ www/protected/components/UserIdentity.php
@@ -15,19 +15,44 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+    private $_id;
+    public $email;
+    const ERROR_EMAIL_INVALID = 'Неправильный email';
+
+    public function __construct($email,$password)
+    {
+        $this->email=$email;
+        $this->password=$password;
+    }
+
+    public function authenticate()
+    {
+        $record=User::model()->findByAttributes(array('email'=>$this->email));
+        if($record===null)
+            $this->errorCode=self::ERROR_EMAIL_INVALID;
+        else if($record->password!==crypt($this->password,$record->password))
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;
+        else
+        {
+            $this->_id = $record->id;
+            $this->setState('id', $record->id);
+            $this->setState('email', $record->email);
+            $this->errorCode=self::ERROR_NONE;
+        }
+        return !$this->errorCode;
+    }
+
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
 }
