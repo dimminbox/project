c22d7fd744e14757d37d197885493e51164fdb71
osadrum <vratar_06@mail.ru>
2013-07-28 16:26:27 +0400

    добавил пользователся admin

---
+++ www/protected/migrations/m130728_121249_alter_table_users_add_user_admin.php
@@ -0,0 +1,30 @@
+<?php
+
+class m130728_121249_alter_table_users_add_user_admin extends CDbMigration
+{
+	public function up()
+	{
+        $this->insert('{{users}}', array(
+            'name' => 'admin',
+            'password' => 'a$naBbVaP.9Zo',
+            'email' => 'admin@project.ru',
+            'role_id' => '1',
+        ));
+	}
+
+	public function down()
+	{
+        $this->delete('{{users}}', array('name' => 'admin'));
+  	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
