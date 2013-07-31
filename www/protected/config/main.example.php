2e09de14b50635fb53449a3631e89dbc549b2e8e
osadrum <osadrum@gmail.com>
2013-07-31 22:12:05 +0400

    обновил конфиги

--- www/protected/config/main.example.php
+++ www/protected/config/main.example.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'My Web Application',
+    'name'=>'Доверительное управление',
     'theme' => 'default',
     'language' => 'ru',
 
@@ -16,14 +16,18 @@ return array(
 
     // autoloading model and component classes
     'import'=>array(
+
         'application.models.*',
         'application.components.*',
+        'application.modules.user.models.*',
+        'application.modules.user.components.*',
+        'application.modules.rights.*',
+        'application.modules.rights.models.*',
+        'application.modules.rights.components.*',
     ),
 
     'modules'=>array(
         // uncomment the following to enable the Gii tool
-        'admin' => array(),
-        'private' => array(),
         'gii'=>array(
             'class'=>'system.gii.GiiModule',
             'password'=>'gii',
@@ -31,15 +35,66 @@ return array(
             'ipFilters'=>array('127.0.0.1','::1'),
         ),
 
+        'user'=>array(
+            'tableUsers' => 'hyip_users',
+            'tableProfiles' => 'hyip_profiles',
+            'tableProfileFields' => 'hyip_profiles_fields',
+            # encrypting method (php hash function)
+            'hash' => 'md5',
+            # send activation email
+            'sendActivationMail' => false,
+            # allow access for non-activated users
+            'loginNotActiv' => false,
+            # activate user on registration (only sendActivationMail = false)
+            'activeAfterRegister' => true,
+            # automatically login from registration
+            'autoLogin' => true,
+            # registration path
+            'registrationUrl' => array('/user/registration'),
+            # recovery password path
+            'recoveryUrl' => array('/user/recovery'),
+            # login form path
+            'loginUrl' => array('/user/login'),
+            # page after login
+            'returnUrl' => array('/user/profile'),
+            # page after logout
+            'returnLogoutUrl' => array('/user/login'),
+        ),
+        'rights'=>array(
+            'superuserName'=>'Admin', // Name of the role with super user privileges.
+            'authenticatedName'=>'Authenticated',  // Name of the authenticated user role.
+            'userIdColumn'=>'id', // Name of the user id column in the database.
+            'userNameColumn'=>'username',  // Name of the user name column in the database.
+            'enableBizRule'=>true,  // Whether to enable authorization item business rules.
+            'enableBizRuleData'=>true,   // Whether to enable data for business rules.
+            'displayDescription'=>true,  // Whether to use item description instead of name.
+            'flashSuccessKey'=>'RightsSuccess', // Key to use for setting success flash messages.
+            'flashErrorKey'=>'RightsError', // Key to use for setting error flash messages.
+
+            'baseUrl'=>'/rights', // Base URL for Rights. Change if module is nested.
+            'layout'=>'rights.views.layouts.main',  // Layout to use for displaying Rights.
+            'appLayout'=>'application.views.layouts.main', // Application layout.
+            'cssFile'=>'rights.css', // Style sheet file to use for Rights.
+            'install'=>false,  // Whether to enable installer.
+            'debug'=>false,
+        ),
+
     ),
 
     // application components
     'components'=>array(
+
         'user'=>array(
+            'class'=>'RWebUser',
             // enable cookie-based authentication
             'allowAutoLogin'=>true,
+            'loginUrl'=>array('/user/login'),
         ),
-        // uncomment the following to enable URLs in path-format
+        'authManager'=>array(
+            'class'=>'RDbAuthManager',
+            'connectionID'=>'db',
+            'defaultRoles'=>array('Authenticated', 'Guest'),
+        ),
 
         'urlManager'=>array(
             'urlFormat'=>'path',
@@ -49,6 +104,13 @@ return array(
                 '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
             ),
         ),
+
+        /* 'user'=>array(
+             // enable cookie-based authentication
+             'class' => 'WebUser',
+             'allowAutoLogin'=>true,
+             'loginUrl' => array('/user/login'),
+         ),*/
 
         /*
 		'db'=>array(
@@ -61,7 +123,7 @@ return array(
             'connectionString' => 'mysql:host=localhost;dbname=project',
             'emulatePrepare' => true,
             'username' => 'root',
-            'password' => '',
+            'password' => '654321',
             'charset' => 'utf8',
             'tablePrefix' => 'hyip_',
         ),
