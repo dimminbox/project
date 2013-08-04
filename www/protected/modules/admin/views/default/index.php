798589ac30ecde0ea5d334112b5f36c660ecedb1
osadrum <osadrum@gmail.com>
2013-08-04 12:58:21 +0400

    русифицировал rights

--- www/protected/modules/admin/views/default/index.php
+++ www/protected/modules/admin/views/default/index.php
@@ -11,7 +11,8 @@ $this->breadcrumbs=array(
 
 <?php
 $this->menu=array(
-    array('label'=>('Управление пользователями'), 'url'=>array('user/admin')),
+    array('label'=>('Управление пользователями'), 'url'=>array('/user/admin')),
+    array('label'=>Yii::t('app','Права доступа'), 'url'=>array('/rights')),
     array('label'=>'Транзакции', 'url'=>array('/admin/userTransaction')),
     array('label'=>'Депозиты', 'url'=>array('/admin/deposit')),
     array('label'=>'Типы депозитов', 'url'=>array('/admin/depositType')),
