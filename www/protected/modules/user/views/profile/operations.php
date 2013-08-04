2545da4e0bfaa7470c11dbefae2904ed7d1521ae
nighthunter83 <yborschev@gmail.com>
2013-08-04 22:34:15 +0400

    Исправление мелких ошибокЭ

--- www/protected/modules/user/views/profile/operations.php
+++ www/protected/modules/user/views/profile/operations.php
@@ -23,7 +23,7 @@ array('label'=>UserModule::t('Logout'), 'url'=>array('/user/logout')),
 
             <tr>
                 <td><?php echo $user->id; ?></td>
-                <td><?php echo strstr($user->time,'.',true); ?></td>
+                <td><?php echo $user->time ?></td>
                 <td><?php echo sprintf("%0.0f", $user->amount_before); ?></td>
 
                 <td><?php
