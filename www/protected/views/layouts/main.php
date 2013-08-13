711ec54b0e9f25793aa3b538c4986785a8a62de5
forjedy5373 <forjedy@gmail.com>
2013-08-13 22:45:00 +0400

    dizain

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -13,7 +13,15 @@
 	<![endif]-->
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/protected/extension/bootstrap/assets/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/protected/extension/bootstrap/assets/css/bootstrap-responsive.css" />
+
+    <script scr = "/protected/extension/bootstrap/assets/js/jquery-2.0.3.js" type = "text/javascript" ></script>
+    <script scr = "/protected/extension/bootstrap/assets/js/bootstrap.js" type = "text/javascript" ></script>
+
+
+
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
