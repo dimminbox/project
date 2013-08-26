ff29db13b545d4c1d49dfbc119d0534c50de3693
osadrum <osadrum@gmail.com>
2013-08-27 00:16:17 +0400

    Обновил авторизацию

--- www/protected/modules/user/views/user/login.php
+++ www/protected/modules/user/views/user/login.php
@@ -3,77 +3,79 @@ $this->pageTitle=Yii::app()->name . ' - '.UserModule::t("Login");
 $this->breadcrumbs=array(
 	UserModule::t("Login"),
 );
+$this->layout='//layouts/login';
 ?>
-
-<h1><?php echo UserModule::t("Login"); ?></h1>
-
-<?php if(Yii::app()->user->hasFlash('loginMessage')): ?>
-
-<div class="success">
-	<?php echo Yii::app()->user->getFlash('loginMessage'); ?>
-</div>
-
-<?php endif; ?>
-
-<p><?php echo UserModule::t("Please fill out the following form with your login credentials:"); ?></p>
-
-<div class="form">
-<?php echo CHtml::beginForm(); ?>
-
-	<p class="note"><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p>
-	
-	<?php echo CHtml::errorSummary($model); ?>
-	
-	<div class="row">
-		<?php echo CHtml::activeLabelEx($model,'username'); ?>
-		<?php echo CHtml::activeTextField($model,'username') ?>
-	</div>
-	
-	<div class="row">
-		<?php echo CHtml::activeLabelEx($model,'password'); ?>
-		<?php echo CHtml::activePasswordField($model,'password') ?>
-	</div>
-	
-	<div class="row">
-		<p class="hint">
-		<?php echo CHtml::link(UserModule::t("Register"),Yii::app()->getModule('user')->registrationUrl); ?> | <?php echo CHtml::link(UserModule::t("Lost Password?"),Yii::app()->getModule('user')->recoveryUrl); ?>
-		</p>
-	</div>
-	
-	<div class="row rememberMe">
-		<?php echo CHtml::activeCheckBox($model,'rememberMe'); ?>
-		<?php echo CHtml::activeLabelEx($model,'rememberMe'); ?>
-	</div>
-
-	<div class="row submit">
-		<?php echo CHtml::submitButton(UserModule::t("Login")); ?>
-	</div>
-	
-<?php echo CHtml::endForm(); ?>
-</div><!-- form -->
-
 
 <?php
-$form = new CForm(array(
-    'elements'=>array(
-        'username'=>array(
-            'type'=>'text',
-            'maxlength'=>32,
-        ),
-        'password'=>array(
-            'type'=>'password',
-            'maxlength'=>32,
-        ),
-        'rememberMe'=>array(
-            'type'=>'checkbox',
-        )
-    ),
-
-    'buttons'=>array(
-        'login'=>array(
-            'type'=>'submit',
-            'label'=>'Login',
-        ),
-    ),
-), $model);
+$this->pageTitle=Yii::app()->name . ' - '.UserModule::t("Login");
+$this->breadcrumbs=array(
+    UserModule::t("Login"),
+);
+$this->layout='//layouts/login';
 ?>
+<div class="container" >
+    <div class="account-container stacked">
+        <div class="content clearfix" align="center">
+        <h1><?php echo UserModule::t("Login"); ?></h1>
+            <div class="login-fields"><p>
+                    <p><?php echo UserModule::t("Please fill out the following form with your login credentials:"); ?></p>
+                        <?php echo CHtml::beginForm(); ?>
+
+                    <p><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p>
+
+                    <?php echo CHtml::errorSummary($model); ?>
+
+                    <div>
+                        <?php echo CHtml::activeLabelEx($model,'username'); ?>
+                        <?php echo CHtml::activeTextField($model,'username') ?>
+                    </div>
+
+                    <div>
+                        <?php echo CHtml::activeLabelEx($model,'password'); ?>
+                        <?php echo CHtml::activePasswordField($model,'password') ?>
+                    </div>
+
+                    <div>
+                        <p class="hint">
+                            <?php echo CHtml::link(UserModule::t("Register"),Yii::app()->getModule('user')->registrationUrl); ?> | <?php echo CHtml::link(UserModule::t("Lost Password?"),Yii::app()->getModule('user')->recoveryUrl); ?>
+                        </p>
+                    </div>
+
+                    <div>
+                        <?php echo CHtml::activeCheckBox($model,'rememberMe'); ?>
+                        <?php echo CHtml::activeLabelEx($model,'rememberMe'); ?>
+                    </div>
+
+                    <div>
+                        <button class="button btn btn-warning btn-large">
+                            <?php echo UserModule::t("Login"); ?>
+                        </button>
+
+                    </div>
+
+                    <?php echo CHtml::endForm(); ?>
+                </div><!-- form -->
+
+
+                   </div>
+                <!-- .actions -->
+            </form></div>
+        <!-- /content -->
+    </div>
+</div>
+    <!-- /account-container -->
+    <!-- /login-extra -->
+
+    <!-- Le javascript
+    ================================================== -->
+
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/libs/jquery-1.8.3.min.js"></script><script src="./js/libs/jquery-ui-1.10.0.custom.min.js"></script><script src="./js/libs/bootstrap.min.js"></script><script src="./js/Application.js"></script><script src="./js/signin.js"></script><a id="back-to-top" href="#top" style="display: none;"><i class="icon-chevron-up"></i></a></body></html>
+
+
+
+
+
+
+
+<div class="form">
