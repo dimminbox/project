f8b6a8c289afebb30b2a574b197ca088a42bbe3a
osadrum <osadrum@gmail.com>
2013-08-27 21:30:21 +0400

    Обновил вход на сайт

--- www/protected/modules/user/views/recovery/recovery.php
+++ www/protected/modules/user/views/recovery/recovery.php
@@ -3,8 +3,11 @@ $this->breadcrumbs=array(
 	UserModule::t("Login") => array('/user/login'),
 	UserModule::t("Restore"),
 );
+$this->layout = '//layouts/login';
 ?>
-
+<div class="container" >
+    <div class="account-container stacked">
+    <div class="content clearfix" >
 <h1><?php echo UserModule::t("Restore"); ?></h1>
 
 <?php if(Yii::app()->user->hasFlash('recoveryMessage')): ?>
@@ -18,16 +21,20 @@ $this->breadcrumbs=array(
 
 	<?php echo CHtml::errorSummary($form); ?>
 	
-	<div class="row">
-		<?php echo CHtml::activeLabel($form,'login_or_email'); ?>
-		<?php echo CHtml::activeTextField($form,'login_or_email') ?>
+	<div>
+		<?php echo CHtml::activeTextField($form,'login_or_email',array('placeholder' => 'Логин или email')); ?>
 		<p class="hint"><?php echo UserModule::t("Please enter your login or email addres."); ?></p>
 	</div>
-	
-	<div class="row submit">
-		<?php echo CHtml::submitButton(UserModule::t("Restore")); ?>
-	</div>
+
+    <div>
+        <button class="button btn btn-warning btn-large">
+            <?php echo UserModule::t("Restore"); ?>
+        </button>
+    </div>
 
 <?php echo CHtml::endForm(); ?>
 </div><!-- form -->
 <?php endif; ?>
+        </div>
+    </div>
+</div>
--- www/themes/tm/views/layouts/login.php
+++ www/themes/tm/views/layouts/login.php
@@ -1,33 +1,29 @@
 <!DOCTYPE html>
 
-<html lang="ru">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html lang="ru"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
 
     <title></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
     <?php Yii::app()->bootstrap->register(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
-    <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/font-awesome.min.css"
-          rel="stylesheet">
-    <link
-        href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/ui-lightness/jquery-ui-1.10.0.custom.min.css"
-        rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/ui-lightness/jquery-ui-1.10.0.custom.min.css" rel="stylesheet">
     <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/base-admin-2.css" rel="stylesheet">
-    <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/base-admin-2-responsive.css"
-          rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/base-admin-2-responsive.css" rel="stylesheet">
     <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/dashboard.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/signin.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/public/tm/profile/css/bootstrap-responsive.min.css" rel="stylesheet">
 
     <?php Yii::app()->clientScript->registerCoreScript('cookie'); ?>
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-    <style type="text/css"></style>
-</head>
+    <style type="text/css"></style></head>
 
 <body>
 
@@ -45,24 +41,23 @@
             <a class="brand" href="<?php echo Yii::app()->createAbsoluteUrl('/site/index') ?>">
                 Логотип
             </a>
-
             <div class="stat stat-time">
-                <span class="stat-value"><?php echo date('H:i:s') ?></span>
+                <span class="stat-value"><?php echo date('H:i:s')?></span>
             </div>
 
 
+
+
+
+
+
         </div>
-        <!-- /container -->
 
-
-    </div>
-    <!-- /navbar-inner -->
+    </div> <!-- /navbar-inner -->
     <div class="container">
 
         <?php echo $content; ?>
 
-    </div>
-    <!-- /container -->
+    </div> <!-- /container -->
 
-</div>
-<!-- /navbar -->
+</div> <!-- /navbar -->
