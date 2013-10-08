07fd59d753291ea990efc4c4e95ed8e3f38d7e83
nighthunter83 <yborschev@gmail.com>
2013-10-08 20:18:19 +0400

    обновление

--- www/protected/components/Sms.php
+++ www/protected/components/Sms.php
@@ -2,11 +2,12 @@
 
 class Sms extends CComponent {
 
-    const API_URL = 'http://smspilot.ru/api.php';
-    const API_KEY = 'KE65A0GA2U1J4874MH3467PI6KI3NT2KX7OO8BA24545EF5036104I52276R6E5S';
+    const API_URL = 'http://sms.ru/sms/send';
+    const API_KEY = '7bbeb875-25fb-55d4-d1de-ff18d53b3bbf';
+    const FROM = 'cronofunds';
 
     public static function send($to, $message) {
-        $url = self::API_URL . '?' . 'send=' . urlencode($message) . '&to=' . $to . '&from=TestSMS&apikey=' . self::API_KEY;
+        $url = self::API_URL . '?' . 'api_id=' . self::API_KEY . '&to=' . $to . '&from=' . self::FROM . '&text=' . urlencode($message);
         $result = file_get_contents($url);
         if ( false === $result ) {
             return false;
